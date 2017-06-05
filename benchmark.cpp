d7f2b6e0194e6f2219a297e9642e456a960e3f4b
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-06-05 17:36:07 +0100

    added random mode/benchmarks

--- benchmark.cpp
+++ benchmark.cpp
@@ -6,9 +6,9 @@
 #include <benchmark/benchmark.h>
 
 template<typename Container>
-Container generate(size_t values)
+Container generate(size_t values, bool random = false)
 {
-	Word word(values);
+	Word word(values, random);
 	Container container;
 	while(word.Next())
 		container.insert(word.Value);
@@ -16,10 +16,10 @@ Container generate(size_t values)
 }
 
 template<typename Container>
-static void insert(benchmark::State& state)
+static void insert(benchmark::State& state, bool random = false)
 {
 	Container set;
-	Word word;
+	Word word(Word::NoLimit, random);
 	while(state.KeepRunning())
 	{
 		word.Next();
@@ -28,10 +28,10 @@ static void insert(benchmark::State& state)
 }
 
 template<typename Container>
-static void lookup_n(benchmark::State& state, size_t values)
+static void lookup_n(benchmark::State& state, size_t values, bool random = false)
 {
 	auto container = generate<Container>(values);
-	Word word;
+	Word word(Word::NoLimit, random);
 	while(state.KeepRunning())
 	{
 		word.Next();
@@ -56,6 +56,18 @@ static void ordered_lookup_large(benchmark::State& state)
 { lookup_n<Ordered>(state, 1000000);  }
 BENCHMARK(ordered_lookup_large);
 
+static void ordered_insert_random(benchmark::State& state)
+{ insert<Ordered>(state, true); }
+BENCHMARK(ordered_insert_random);
+
+static void ordered_lookup_small_random(benchmark::State& state)
+{ lookup_n<Ordered>(state, 100, true);  }
+BENCHMARK(ordered_lookup_small_random);
+
+static void ordered_lookup_large_random(benchmark::State& state)
+{ lookup_n<Ordered>(state, 1000000, true);  }
+BENCHMARK(ordered_lookup_large_random);
+
 
 using Unordered = std::unordered_set<std::string>;
 
@@ -72,6 +84,18 @@ static void unordered_lookup_large(benchmark::State& state)
 { lookup_n<Unordered>(state, 1000000);  }
 BENCHMARK(unordered_lookup_large);
 
+static void unordered_insert_random(benchmark::State& state)
+{ insert<Unordered>(state, true); }
+BENCHMARK(unordered_insert_random);
+
+static void unordered_lookup_small_random(benchmark::State& state)
+{ lookup_n<Unordered>(state, 100, true);  }
+BENCHMARK(unordered_lookup_small_random);
+
+static void unordered_lookup_large_random(benchmark::State& state)
+{ lookup_n<Unordered>(state, 1000000, true);  }
+BENCHMARK(unordered_lookup_large_random);
+
 
 using CustomUnordered = std::unordered_set<std::string, mul_shift_hash<std::string>>;
 
@@ -87,6 +111,18 @@ BENCHMARK(custom_unordered_lookup_small);
 static void custom_unordered_lookup_large(benchmark::State& state)
 { lookup_n<CustomUnordered>(state, 1000000);  }
 BENCHMARK(custom_unordered_lookup_large);
+
+static void custom_unordered_insert_random(benchmark::State& state)
+{ insert<CustomUnordered>(state, true); }
+BENCHMARK(custom_unordered_insert_random);
+
+static void custom_unordered_lookup_small_random(benchmark::State& state)
+{ lookup_n<CustomUnordered>(state, 100, true);  }
+BENCHMARK(custom_unordered_lookup_small_random);
+
+static void custom_unordered_lookup_large_random(benchmark::State& state)
+{ lookup_n<CustomUnordered>(state, 1000000, true);  }
+BENCHMARK(custom_unordered_lookup_large_random);
 
 
 BENCHMARK_MAIN();
--- word.h
+++ word.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <stdlib.h>
 
 static constexpr char FirstChar = ' ';
 static constexpr char LastChar = 0x7f;
@@ -22,16 +23,30 @@ struct Word
 {
 	std::string Value;
 	size_t Limit;
+	bool Random;
+	static constexpr size_t RandomChars = 8;
+	static constexpr size_t NoLimit = (size_t)~0;
 
-	Word(size_t limit = (size_t)~0): Limit(limit)
+	Word(size_t limit = NoLimit, bool random = false): Limit(limit), Random(random)
 	{
-		Value.reserve(5);
+		if (random)
+			Value.resize(RandomChars);
+		else
+			Value.reserve(5);
 	}
 
 	bool Next()
 	{
 		if (Limit-- == 0)
 			return false;
+
+		if (Random)
+		{
+			size_t n = RandomChars;
+			for(size_t i = 0 ; i < n; ++i)
+				Value[n] = FirstChar + (random() * size_t(LastChar - FirstChar) / RAND_MAX);
+			return true;
+		}
 
 		ssize_t n = Value.size() - 1;
 		while(n >= 0) {
