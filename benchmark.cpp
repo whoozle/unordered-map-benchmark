ee1360de9778ecf232f1e69822bfc6841ed605b2
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-06-05 16:01:02 +0100

    reordered tests, converted lookup tests to single template

--- benchmark.cpp
+++ benchmark.cpp
@@ -4,9 +4,6 @@
 
 #include <benchmark/benchmark.h>
 
-using Ordered = std::set<std::string>;
-using Unordered = std::unordered_set<std::string>;
-
 template<typename Container>
 Container generate(size_t values)
 {
@@ -17,9 +14,10 @@ Container generate(size_t values)
 	return container;
 }
 
-static void ordered_insert(benchmark::State& state)
+template<typename Container>
+static void insert(benchmark::State& state)
 {
-	Ordered set;
+	Container set;
 	Word word;
 	while(state.KeepRunning())
 	{
@@ -27,8 +25,6 @@ static void ordered_insert(benchmark::State& state)
 		set.insert(word.Value);
 	}
 }
-
-BENCHMARK(ordered_insert);
 
 template<typename Container>
 static void lookup_n(benchmark::State& state, size_t values)
@@ -42,33 +38,37 @@ static void lookup_n(benchmark::State& state, size_t values)
 		benchmark::DoNotOptimize(i);
 	}
 }
+
+
+using Ordered = std::set<std::string>;
+
+
+static void ordered_insert(benchmark::State& state)
+{ insert<Ordered>(state); }
+BENCHMARK(ordered_insert);
+
 static void ordered_lookup_small(benchmark::State& state)
 { lookup_n<Ordered>(state, 100);  }
+BENCHMARK(ordered_lookup_small);
+
 static void ordered_lookup_large(benchmark::State& state)
 { lookup_n<Ordered>(state, 1000000);  }
-
-BENCHMARK(ordered_lookup_small);
 BENCHMARK(ordered_lookup_large);
 
-static void unordered_insert(benchmark::State& state)
-{
-	std::unordered_set<std::string> set;
-	Word word;
-	while(state.KeepRunning())
-	{
-		word.Next();
-		set.insert(word.Value);
-	}
-}
 
+using Unordered = std::unordered_set<std::string>;
+
+
+static void unordered_insert(benchmark::State& state)
+{ insert<Unordered>(state); }
 BENCHMARK(unordered_insert);
 
 static void unordered_lookup_small(benchmark::State& state)
 { lookup_n<Unordered>(state, 100);  }
+BENCHMARK(unordered_lookup_small);
+
 static void unordered_lookup_large(benchmark::State& state)
 { lookup_n<Unordered>(state, 1000000);  }
-
-BENCHMARK(unordered_lookup_small);
 BENCHMARK(unordered_lookup_large);
 
 BENCHMARK_MAIN();
